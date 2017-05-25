9dc8660d5877840e8202d7cea4d303efe27501bc
Matthew Baggett <matthew@baggett.me>
2017-05-25 11:10:19 +0100

    Initial commit

---
+++ InflectExtension.php
@@ -0,0 +1,34 @@
+<?php
+namespace Gone\Twig;
+
+use Gone\Inflection\Inflect;
+
+class InflectExtension extends \Twig_Extension
+{
+    public function getFilters()
+    {
+        return array(
+            'singular' => new \Twig_SimpleFilter('singular', [$this, 'singularFilter']),
+            'plural'   => new \Twig_SimpleFilter('plural', [$this, 'pluralFilter']),
+        );
+    }
+
+    public function singularFilter($word)
+    {
+        return Inflect::singularize($word);
+    }
+
+    public function pluralFilter($word, $count = null)
+    {
+        if($count){
+            return Inflect::pluralizeIf($word, $count);
+        }else {
+            return Inflect::pluralize($word);
+        }
+    }
+
+    public function getName()
+    {
+        return 'inflect_extension';
+    }
+}
